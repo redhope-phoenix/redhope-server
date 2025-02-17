745b078cc9f9b00ecae6d16835fbe8eb566e01b9
hostelbetting <hostelbetting.fun@gmail.com>
2025-02-18 01:57:53 +0530

    Update email transporter configuration to use correct SMTP host

--- src/utils/send-email.js
+++ src/utils/send-email.js
@@ -6,9 +6,9 @@ export const sendNodeEmail = async ({ mailTo, subject, html }) => {
 
     // Create a transporter
     const transporter = nodemailer.createTransport({
-        host: 'gmail',
-        secure: true,
+        host: 'smtp.gmail.com',
         port: 465,
+        secure: true,
         auth: {
             user: process.env.NODEMAILER_USER,
             pass: process.env.NODEMAILER_PASS
