e7dd56d276eb48c5c84e67853fe20f5a0d33676c
Sibsankar-de <sibsankarde910@gmail.com>
2025-04-14 23:12:45 +0530

    Enable cron scheduling in app initialization

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(express.static('public'))
 
-// scheduleCron();
+scheduleCron();
 
 app.get("/ping", (req, res) => {
     res.send("Ping successful");
