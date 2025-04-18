030a6e94581c953682a4c26af270b4977a7c4c6a
Abhishek-Kr-Pandit <abhishekrpandit18@gmail.com>
2025-04-18 13:20:51 +0530

    More models added

---
+++ src/models/otp.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import { sendSms } from "../utils/sms-service.js";
+
+const otpSchema = new mongoose.Schema({
+    phoneNo: {
+        type: Number,
+        required: true
+    },
+    otpCode: {
+        type: String,
+        required: true,
+    },
+    expiresAt: {
+        type: Date,
+        required: true,
+        index: { expires: 0 },
+    },
+    isUsed: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+
+// hash otp
+otpSchema.pre("save", async function (next) {
+    if (!this.isModified('otpCode')) next()
+    sendSms(`Your redhope One-time verification code is - ${this.otpCode}. Use the code within 10 minutes. Do not share the otp to others`)
+    this.otpCode = await bcrypt.hash(this.otpCode, 10);
+    next();
+})
+
+// check Otp
+otpSchema.methods.checkOtp = async function (otp) {
+    return await bcrypt.compare(this.otpCode, otp);
+}
+
+export const Otp = mongoose.model('Otp', otpSchema);
---
+++ src/models/request.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema({
+    requestedBy: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    applicant: {
+        type: String,
+        required: true
+    },
+    phoneNo: {
+        type: String,
+        required: true
+    },
+    reason: {
+        type: String
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    bloodGroup: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: Object,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    isFulfilled: {
+        type: Boolean,
+        default: false
+    },
+    reachedTo: {
+        type: Number,
+        default: 0
+    },
+    approvedBy: {
+        type: Number,
+        default: 0
+    },
+    report: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true })
+
+export const Request = mongoose.model("Request", requestSchema);
